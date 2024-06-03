0bf3d9d50e30926dcdc0185717e1465b22f485f8
jiabailong1 <jiabailong1@jd.com>
2024-06-03 15:22:32 +0800

    Initial Commit

---
+++ manyou/manyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  manyouApp.swift
+//  manyou
+//
+//  Created by jiabailong1 on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct manyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
